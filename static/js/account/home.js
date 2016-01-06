ce65a412fd617192960d425687f7d8203d8238cc
jiamin <rsj217@gmail.com>
2016-01-06 17:08:35 +0800

    更改 home 配置

---
+++ static/js/account/home.js
@@ -0,0 +1,10 @@
+var Vue = require('vue');
+var swal = require('sweetalert');
+var utils = require('../utils.js');
+
+var jQuery = $ = require('jquery');
+
+window.jQuery = jQuery;
+require('bootstrap');
+
+require('../../css/sweetalert.css');
